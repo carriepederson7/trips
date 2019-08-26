5e275bc4adacea3ca58ae235da3b42aa642cac8c
Student <example@example.com>
2019-08-26 14:18:39 -0700

    Add back-end and front-end logic.

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,35 @@
+
+// BACK END
+function PlacesVisited() {
+  this.locations = []
+}
+
+function Cities(city, country, landmarksVisited, whenVisited) {
+    this.city = city;
+    this.country = country;
+    this.landmarksVisited = landmarksVisited;
+    this.whenVisited = whenVisited;
+}
+
+PlacesVisited.prototype.addCity = function(location) {
+  this.locations.push(location);
+}
+
+var placesVisited = new PlacesVisited();
+
+
+
 // FRONT END
 $(document).ready(function() {
   $('.form').submit(function(event) {
     event.preventDefault();
+    var city = $("#city").val();
+    var country = $("#country").val();
+    var landmarksVisited = $("#landmarks").val();
+    var whenVisited = $("#whenVisited").val();
+    var newCity = new Cities(city, country, landmarksVisited, whenVisited);
+    placesVisited.addCity(newCity);
+    console.log(placesVisited);
 
   });
 });
-
-// BACK END
-function Locations() {
-  this.city = []
-}
-
-function Cities(country, landmarksVisited, whenVisited) {
-  this.country = country,
-  this.landmarksVisited = landmarksVisited,
-  this.whenVisited = whenVisited
-}
